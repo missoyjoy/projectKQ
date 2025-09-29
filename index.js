784ff65cefed1816dea3cee0e6c145110832f55f
JOYMISSOY <missoyjoy2019@gmail.com>
2025-09-29 15:55:53 +0300

    fetched

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 // Auto update year in footer
 document.getElementById("year").textContent = new Date().getFullYear();
-// Update year automatically
-document.getElementById("year").textContent = new Date().getFullYear();
-
-// Mobile menu toggle
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
+fetch('https://raw.githubusercontent.com/missoyjoy/projectKQ/1a10b822474733d1c2b353bd5e934fd0994bde9a/db/db.json')
+  .then(res => res.json())
+  .then(data => console.log(data));
