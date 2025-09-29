e966157a5e7a1789163d0b5d45774151e06d268c
JOYMISSOY <missoyjoy2019@gmail.com>
2025-09-29 09:40:56 +0300

    hambuger

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 
 
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
