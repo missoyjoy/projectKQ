eacd643254adab5d0fb804a680147d2825090f92
JOYMISSOY <missoyjoy2019@gmail.com>
2025-09-29 09:58:12 +0300

    responsive

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Auto update year in footer
 document.getElementById("year").textContent = new Date().getFullYear();
+// Update year automatically
+document.getElementById("year").textContent = new Date().getFullYear();
 
-
-
+// Mobile menu toggle
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
